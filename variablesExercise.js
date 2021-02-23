f0c4fd3670744d9ef213f3713e257ae543e28a34
Alisha Bolin <alishabolin@KCCCs-MBP.lan>
2021-02-23 12:45:39 -0600

    revised last bonus answer

--- variablesExercise.js
+++ variablesExercise.js
@@ -1,22 +1,31 @@
+// VARIABLES EXERCISE
+
 // 1) Variable that doesn't change: const
 
 // 2) 
     let num = 20;
+    console.log(num);
 
 // 3)
     num = 15;
+    console.log(num);
 
 // 4)
-    num = num + 7;
+    // num = num + 7;
+    num += 7;
+    console.log(num);
+
 
 // 5)
     num--;
+    console.log(num);
 
 // 6)
     num = "This is not a number";
+    console.log(num);
 
 // 7) 
-    let blank = undefined;
+    let blank;
 
 // 8)
     console.log(blank);
@@ -31,6 +40,10 @@
 // 1)
     const [quarter, dime, nickel, penny] = [0.25, 0.10, 0.05, 0.01];
 
+    /* also
+    const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+    */
+
 // 2)
     console.log(quarter, dime, nickel, penny);
 
@@ -38,6 +51,5 @@
     let letters = "qwertyuiop";
 
 // 4)
-    console.log(letters.length);
-    console.log(letters[9]);
-    
+    // console.log(letters[letters.length - 1]);
+    console.log(letters[letters.length - 1]);    
